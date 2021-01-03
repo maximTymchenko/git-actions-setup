f9015a123f9ae52998eed92d2d51263d4608fb04
maximTymchenko <maximTymchenko>
2021-01-03 13:20:50 +0200

    test

--- src/App.js
+++ src/App.js
@@ -7,8 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. Yarn git actions
-          develop branch
+          Edit <code>src/App.js</code> and save to reload. Test without region and name
         </p>
         <a
           className="App-link"
