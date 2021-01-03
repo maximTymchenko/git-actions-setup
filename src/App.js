799bf146a420f8ba286e00174e8905413670037e
maximTymchenko <maximTymchenko>
2021-01-03 13:23:02 +0200

    test2

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. Test without region and name
+          Edit <code>src/App.js</code> and save to reload. Test without region and bucket name
         </p>
         <a
           className="App-link"
