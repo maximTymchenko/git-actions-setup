4b32f146bad1d0f4659d644ed4131c84a97e861f
maximTymchenko <maximTymchenko>
2021-02-09 13:03:52 +0200

    resize main page

--- src/components/pagesComponents/MainPage/Technologies/Technologies.tsx
+++ src/components/pagesComponents/MainPage/Technologies/Technologies.tsx
@@ -1,61 +1,62 @@
-import React from 'react';
-import { Link } from 'gatsby';
-import './Technologies.scss';
-import Button from '../../../common/Button';
-import SectionTitle from '../../../common/SectionTitle';
-import arrowRight from '../../../../assets/img/arr-right.svg';
-import TechIcon from '../TechIcon/TechIcon';
-import { FaReact, FaNode, FaJava, FaHtml5 } from 'react-icons/fa';
-import { SiRails } from 'react-icons/si';
-import { DiJavascript1 } from 'react-icons/di';
+import React from 'react'
+import { Link } from 'gatsby'
+import './Technologies.scss'
+import Button from '../../../common/Button'
+import SectionTitle from '../../../common/SectionTitle'
+import arrowRight from '../../../../assets/img/arr-right.svg'
+import TechIcon from '../TechIcon/TechIcon'
+import { FaReact, FaNode, FaJava, FaHtml5 } from 'react-icons/fa'
+import { SiRails } from 'react-icons/si'
+import { DiJavascript1 } from 'react-icons/di'
 
 const Technologies: React.FC = () => {
-    const techIcons = [
-        <FaHtml5 />,
-        <FaNode />,
-        <SiRails />,
-        <FaJava />,
-        <DiJavascript1 />,
-        <FaReact />,
-    ];
+  const techIcons = [
+    <FaHtml5 />,
+    <FaNode />,
+    <SiRails />,
+    <FaJava />,
+    <DiJavascript1 />,
+    <FaReact />,
+  ]
 
-    return (
-        <div data-testid="technologies" className="tech">
-            <div className="tech__info">
-                <SectionTitle
-                    type={'relevant'}
-                    className="title"
-                    title={
-                        'Cannot find relevant technology stack or similar project from your industry?'
-                    }
-                />
-                <p className="tech__desc">
-                    It must be our rigorous
-                    <span className="tech__protection">
-                        intellectual property protection effort.
-                    </span>
-                    Just tell us what you need and get that information in less than 24 hours.
-                </p>
-                <Link className="link" to="/contact-us">
-                    <Button
-                        icon={arrowRight}
-                        type={'primary'}
-                        className="button"
-                        Tell
-                        us
-                        desc={'Tell us'}
-                    >
-                        Tell us
-                    </Button>
-                </Link>
-            </div>
-            <div className="tech__images">
-                <div className="tech__images-wrapper">
-                    <TechIcon iconsArray={techIcons} type="main" />
-                </div>
-            </div>
+  return (
+    <div data-testid="technologies" className="tech">
+      <div className="tech__info">
+        <SectionTitle
+          type="relevant"
+          className="title"
+          title={
+            'Cannot find relevant technology stack or similar project from your industry?'
+          }
+        />
+        <p className="tech__desc">
+          It must be our rigorous
+          <span className="tech__protection">
+            intellectual property protection effort.
+          </span>
+          Just tell us what you need and get that information in less than 24
+          hours.
+        </p>
+        <Link className="link" to="/contact-us">
+          <Button
+            icon={arrowRight}
+            type={'primary'}
+            className="button"
+            Tell
+            us
+            desc={'Tell us'}
+          >
+            Tell us
+          </Button>
+        </Link>
+      </div>
+      <div className="tech__images">
+        <div className="tech__images-wrapper">
+          <TechIcon iconsArray={techIcons} type="main" />
         </div>
-    );
-};
+      </div>
+    </div>
+  )
+}
 
-export default Technologies;
+export default Technologies
