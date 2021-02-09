8c9d87c5b49c987d2c53fcd04dbdb26bfed823f1
maximTymchenko <maximTymchenko>
2021-02-09 12:40:33 +0200

    edit main page nav

--- src/components/common/Nav/Nav.tsx
+++ src/components/common/Nav/Nav.tsx
@@ -1,73 +1,73 @@
-import React, { useState, useRef, useEffect } from 'react';
-import { Link } from 'gatsby';
-import './Nav.scss';
-import { INavLinks } from '../PrimaryNav';
-import Button from '../Button';
-import arrLeft from '../../../assets/img/arr-left.png';
+import React, { useState, useRef, useEffect } from 'react'
+import { Link } from 'gatsby'
+import './Nav.scss'
+import { INavLinks } from '../PrimaryNav'
+import Button from '../Button'
+import arrLeft from '../../../assets/img/arr-left.png'
 
 const Nav = ({ links, path, type }) => {
-    const [navLinks, setNavLinks] = useState<INavLinks>(links);
+  const [navLinks, setNavLinks] = useState<INavLinks>(links)
 
-    const toggleNavClass = (index: number) => {
-        setNavLinks({ ...navLinks, activeLink: navLinks.links[index] });
-    };
+  const toggleNavClass = (index: number) => {
+    setNavLinks({ ...navLinks, activeLink: navLinks.links[index] })
+  }
 
-    const toggleActiveClass = (index: number) => {
-        if (navLinks.links[index] === navLinks.activeLink) {
-            return 'nav__link nav__link--active';
-        } else {
-            return 'nav__link';
-        }
-    };
+  const toggleActiveClass = (index: number) => {
+    if (navLinks.links[index] === navLinks.activeLink) {
+      return 'nav__link nav__link--active'
+    } else {
+      return 'nav__link'
+    }
+  }
 
-    const linkRef = useRef();
+  //   const linkRef = useRef()
 
-    const [linkState, setLinkState] = useState<boolean>(false);
+  //   const [linkState, setLinkState] = useState<boolean>(false)
 
-    const getElemClass = () => {
-        const linkClasses = linkRef.current;
-        // console.log();
+  //   const getElemClass = () => {
+  //     const linkClasses = linkRef.current
+  //     // console.log();
 
-        if (linkClasses.classList.contains('active__border--style')) {
-            setLinkState(true);
-            console.log('YES');
-        }
-    };
+  //     if (linkClasses.classList.contains('active__border--style')) {
+  //       setLinkState(true)
+  //       console.log('YES')
+  //     }
+  //   }
 
-    console.log(linkRef);
+  //   console.log(linkRef)
 
-    return (
-        <ul className={`nav nav--${type}`}>
-            <div className="nav__container">
-                {/* <Button icon={arrLeft} type="apply">
+  return (
+    <ul className={`nav nav--${type}`}>
+      <div className="nav__container">
+        {/* <Button icon={arrLeft} type="apply">
                     Back
                 </Button> */}
-                {navLinks.links.map((item, index) => (
-                    <Link
-                        ref={linkRef}
-                        data-testid="nav"
-                        activeClassName="active__border--style"
-                        to={`${item.page}`}
-                        className="link__margin  active__border nav__link"
-                        state={{ choice: path }}
-                        // onClick={getElemClass}
-                    >
-                        {/* <li
-                            onClick={() => {
-                                toggleNavClass(index);
-                            }}
-                            className={toggleActiveClass(index)}
-                            key={index}
-                        >
-                            {item.title}
-                        </li> */}
+        {navLinks.links.map((item, index) => (
+          <Link
+            // ref={linkRef}
+            data-testid="nav"
+            activeClassName="active__border--style"
+            to={`${item.page}`}
+            className="link__margin  active__border nav__link"
+            state={{ choice: path }}
+            // onClick={getElemClass}
+          >
+            <li
+              onClick={() => {
+                toggleNavClass(index)
+              }}
+              className={toggleActiveClass(index)}
+              key={index}
+            >
+              {item.title}
+            </li>
 
-                        {item.title}
-                    </Link>
-                ))}
-            </div>
-        </ul>
-    );
-};
+            {/* {item.title} */}
+          </Link>
+        ))}
+      </div>
+    </ul>
+  )
+}
 
-export default Nav;
+export default Nav
