d76365898db98c1823eecc3d804f939cd71fdadb
maximTymchenko <maximTymchenko>
2021-02-18 12:27:58 +0200

    changed blog cards image

--- src/pages/blog/Blog.tsx
+++ src/pages/blog/Blog.tsx
@@ -5,8 +5,8 @@ import BlogHeader from '../../components/pagesComponents/Blog/BlogHeader';
 import BlogFilter from '../../components/pagesComponents/Blog/BlogFilter';
 import LargeCompanyContainer from '../../components/pagesComponents/MainPage/LargeCompanyContainer';
 import SmallCompanyContainer from '../../components/pagesComponents/MainPage/SmallCompanyContainer';
-import walmart from '../../assets/img/companies/walmart.svg';
-import statflo from '../../assets/img/companies/statflo.svg';
+import Button from '../../components/common/Button';
+import arrDown from '../../assets/img/arr-down.png';
 
 const Blog: FC = () => {
     return (
@@ -23,34 +23,61 @@ const Blog: FC = () => {
                     <div className="blog__cards-wrapper">
                         <div className="blog__card">
                             <LargeCompanyContainer
-                                typeBG="bg-rooms"
-                                img={walmart}
+                                logoPos="hide"
+                                typeBG="bg-server-room"
                                 description="This is how to migrate from AWS to Google Cloud and save some serious cash"
                                 label="EDITORIAL"
-                                logoPos="mb-more"
                                 // hashtags="Microservice architecture, Microsoft HoloLens 2, REST API, Web development, +13 more"
                             />
                         </div>
                         <div className="blog__card">
                             <SmallCompanyContainer
-                                type="secondary"
+                                type="bg-server-connect"
                                 size="blog"
-                                img={statflo}
                                 description="This is how to migrate from AWS to Google Cloud and save some serious cash"
                                 logoType="secondary"
                                 label="EDITORIAL"
+                                logoType="hide"
+
                                 // hashtags="Microservice architecture, Microsoft HoloLens 2, REST API, Web development, +13 more"
                             />
                         </div>
                     </div>
                     <div className="blog__cards-wrapper">
                         <div className="blog__card">
+                            <LargeCompanyContainer
+                                typeBG="bg-desktop"
+                                description="This is how to migrate from AWS to Google Cloud and save some serious cash "
+                                label="EDITORIAL"
+                                logoPos="hide"
+                                // hashtags="Microservice architecture, Microsoft HoloLens 2, REST API, Web development, +13 more"
+                            />
+                        </div>
+                        <div className="blog__card">
+                            <LargeCompanyContainer
+                                typeBG="bg-phones"
+                                description="This is how to migrate from AWS to Google Cloud and save some serious cash "
+                                label="EDITORIAL"
+                                logoPos="hide"
+                                // hashtags="Microservice architecture, Microsoft HoloLens 2, REST API, Web development, +13 more"
+                            />
+                        </div>
+                    </div>
+                    <LargeCompanyContainer
+                        typeBG="bg-code"
+                        description="This is how to migrate from AWS to Google Cloud and save some serious cash "
+                        // hashtags="Microservice architecture, Microsoft HoloLens 2, REST API, Web development, +13 more"
+                        size="largest"
+                        label="EDITORIAL"
+                        logoPos="hide"
+                    />
+                    <div className="blog__cards-wrapper">
+                        <div className="blog__card">
                             <SmallCompanyContainer
-                                type="secondary"
+                                type="bg-building"
                                 size="blog"
-                                img={statflo}
                                 description="This is how to migrate from AWS to Google Cloud and save some serious cash"
-                                logoType="secondary"
+                                logoType="hide"
                                 label="EDITORIAL"
                                 // hashtags="Microservice architecture, Microsoft HoloLens 2, REST API, Web development, +13 more"
                             />
@@ -58,14 +85,18 @@ const Blog: FC = () => {
                         <div className="blog__card">
                             <LargeCompanyContainer
                                 typeBG="bg-rooms"
-                                img={walmart}
-                                description="This is how to migrate from AWS to Google Cloud and save some serious cash "
+                                description="This is how to migrate from AWS to Google Cloud and save some serious cash"
                                 label="EDITORIAL"
-                                logoPos="mb-more"
+                                logoPos="hide"
                                 // hashtags="Microservice architecture, Microsoft HoloLens 2, REST API, Web development, +13 more"
                             />
                         </div>
                     </div>
+                </div>
+                <div className="blog__btn">
+                    <Button type="primary" subtype="large" icon={arrDown}>
+                        Load more articles
+                    </Button>
                 </div>
             </section>
         </Layout>
