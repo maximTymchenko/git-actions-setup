5de846d2bc1ee77e5ad68a91392561679e66b03e
maximTymchenko <maximTymchenko>
2021-02-24 11:47:53 +0200

    fix calandly button

--- src/components/pagesComponents/MainPage/TextSpan/TextSpan.tsx
+++ src/components/pagesComponents/MainPage/TextSpan/TextSpan.tsx
@@ -16,11 +16,11 @@ const TextSpan: FC = (props: ITextSpan) => {
             <div className="text-span__wrapper">
                 <SectionTitle type={'secondary'} className="title" title={props.title} />
                 <p className="text-span__info">{props.children}</p>
-                <Button type="secondary" desc="Discover all" icon={Calendar}>
-                    <a target="_blank" href="https://calendly.com/yuriidrozd">
+                <a target="_blank" href="https://calendly.com/yuriidrozd">
+                    <Button type="secondary" desc="Discover all" icon={Calendar}>
                         Book a meeting
-                    </a>
-                </Button>
+                    </Button>
+                </a>
             </div>
         </section>
     );
