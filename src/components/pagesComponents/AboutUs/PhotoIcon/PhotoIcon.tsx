076d0a740bc73511d2e4b54874761fda7ed613db
maximTymchenko <maximTymchenko>
2021-03-01 11:44:15 +0200

    add talanted people and office photo

--- src/components/pagesComponents/AboutUs/PhotoIcon/PhotoIcon.tsx
+++ src/components/pagesComponents/AboutUs/PhotoIcon/PhotoIcon.tsx
@@ -7,8 +7,8 @@ const PhotoIcon: FC = (props) => {
     //     dots: true,
     //     infinite: true,
     //     speed: 500,
-    //     slidesToShow: 3,
-    //     slidesToScroll: 3,
+    //     slidesToShow: 1,
+    //     slidesToScroll: 1,
     // };
 
     return (
